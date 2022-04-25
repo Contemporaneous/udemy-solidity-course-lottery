9b98ed6a51e535a43218fd4e20a3fdfa16e67cd6
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-04-26 06:50:05 +1000

    Add pickWinner mechanic

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -10,8 +10,19 @@ contract Lottery {
     }
 
     function enter() payable public {
-        require(msg.value >= 0.01 ether, "You must enter the lottery with at least 0.01 ether");
+        require(msg.value >= 0.01 ether); //, "You must enter the lottery with at least 0.01 ether")
         players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager); //Must be manager
+        require(players.length > 0); //Must have players
+        uint winnerId = random() % players.length;
+        players[winnerId].transfer(this.balance);
     }
 
 }
