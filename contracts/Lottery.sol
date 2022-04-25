3ceb117e747bb51da130c50339411b59d8d25c09
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-04-26 06:18:43 +1000

    Add basic entry mechanic

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,4 +2,16 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
 
+    address public manager;
+    address[] public players;
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() payable public {
+        require(msg.value >= 0.01 ether, "You must enter the lottery with at least 0.01 ether");
+        players.push(msg.sender);
+    }
+
 }
